b298f186b16e2c20fba40415f1a0584cd0689246
Katochimoto <bufpost@yandex.ru>
2013-11-24 01:22:56 +0400

    псевдоэлемент

--- src/blocks/field/field.js
+++ src/blocks/field/field.js
@@ -1,4 +1,4 @@
-(function(xtag, xblocks) {
+(function(xtag, xblocks, Modernizr, tv4) {
     'use strict';
 
     var SCHEMA_REL = 'http://xblocks.ru/xb-field';
@@ -9,21 +9,53 @@
         /* borschik:include:schema.json */
     );
 
-    function onupdate(element) {
-        element.__lock = true;
-        element.removeAttribute('value');
-        element.__controller = xblocks.rootElement(element).querySelector('input,textarea');
-        element.__lock = false;
-    }
-
     xtag.register('xb-field', {
         lifecycle: {
             created: function() {
-                xblocks.log('[field]', 'created', this);
+                this.__xb = xblocks.element.create(this, {
+                    schema: SCHEMA_REL,
+                    defaultAttrs: {
+                        type: 'text',
+                        size: 'm',
+                        rows: 1
+                    }
+                });
+
+                xblocks.log('[field]', 'created', this.__xb);
                 xblocks.log.time('[field] created');
 
-                this.observer.on();
-                xblocks.elementUpdate(this, onupdate);
+
+
+                this.__xb.on('update', function() {
+                    element.__lock = true;
+                    element.removeAttribute('value');
+                    element.__controller = xblocks.rootElement(element).querySelector('input,textarea');
+                    element.__lock = false;
+                });
+
+                this.__xb.on('click', function(event) {
+                    if (this.isLock() || !xblocks.attrs.isEmpty(that, 'disabled')) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        return false;
+                    }
+
+                    if (xtag.hasClass(event.target, 'js-reset')) {
+                        that.__lock = true;
+                        that.__controller.value = '';
+                        that.__lock = false;
+                    }
+
+                    return true;
+                });
+
+                this.__xb.update();
+
+
+
+
+
+                /*xblocks.elementUpdate(this, onupdate);
 
                 var that = this;
                 this.__events = xtag.addEvents(xblocks.rootElement(this), {
@@ -42,97 +74,32 @@
 
                         return true;
                     }
-                });
+                });*/
 
                 xblocks.log.timeEnd('[field] created');
             },
 
+
             inserted: function() {
-                xblocks.log('[field]', 'inserted', this);
+                this.__xb.trigger('inserted');
             },
 
             removed: function() {
-                xblocks.log('[field]', 'removed', this);
-                this.observer.remove();
-                xtag.removeEvents(xblocks.rootElement(this), this.__events);
-                delete this.__events;
-                delete this.__controller;
+                this.__xb.trigger('removed');
             },
 
             attributeChanged: function(attrName, oldValue, newValue) {
-                if (this.__lock) {
-                    xblocks.log('[field]', 'attributeChanged', 'lock', arguments);
-                    return;
-                }
-
-                xblocks.log('[field]', 'attributeChanged', this, arguments);
-                xblocks.elementUpdate(this, onupdate);
+                this.__xb.trigger('attributeChanged', attrName, oldValue, newValue);
             }
         },
 
         accessors: {
-            schema: {
-                get: function() {
-                    return SCHEMA_REL;
-                }
-            },
-
-            defaultAttrs: {
-                get: function() {
-                    return {
-                        'type': 'text',
-                        'size': 'm',
-                        'rows': 1,
-                        'value': this.value
-                    };
-                }
-            },
-
-            styleSource: {
-                get: function() {
-                    return borschik.link('@field.css');
-                }
-            },
-
             value: {
                 get: function() {
                     return this.__controller && this.__controller.value;
                 },
                 set: function(value) {
                     this.__controller.value = value;
-                }
-            },
-
-            observer: {
-                get: function() {
-                    var that = this;
-                    var observer;
-
-                    if (!Modernizr.createshadowroot) {
-                        observer = that.__observer || (that.__observer = new MutationObserver(function() {
-                            xblocks.log('[field]', 'mutation', that);
-                            xblocks.elementUpdate(that, onupdate);
-                        }));
-                    }
-
-                    return {
-                        on: function() {
-                            observer && observer.observe(that, {
-                                childList: true,
-                                subtree: true,
-                                characterData: true
-                            });
-                        },
-
-                        off: function() {
-                            observer && observer.disconnect();
-                        },
-
-                        remove: function() {
-                            observer && observer.disconnect();
-                            delete that.__observer;
-                        }
-                    };
                 }
             }
         },
@@ -146,5 +113,5 @@
         }
     });
 
-})(xtag, xblocks);
+})(xtag, xblocks, Modernizr, tv4);
 
--- src/lib/xblocks/element.js
+++ src/lib/xblocks/element.js
@@ -2,47 +2,107 @@
     'use strict';
 
     xblocks = xblocks || {};
-    var el = xblocks.element = {};
+    xblocks.element = {};
 
-
-    el.html = function(element, html) {
-        xblocks.log('element->html', element, html);
-
-        if (typeof html !== 'undefined') {
-            element.innerHTML = html;
-
-            if (!Modernizr.createshadowroot) {
-                xblocks.elementUpdate(element);
-            }
-        }
-
-        var content;
-        if (!Modernizr.createshadowroot) {
-            content = element.querySelector('content');
-        }
-
-        return content && content.innerHTML || element.innerHTML;
+    xblocks.element.create = function(node, params) {
+        return new XBElement(node, params);
     };
 
-    el.update = function(element, onupdate) {
-        xblocks.log('element->update', element);
-        xblocks.log.time('element->update');
 
-        element.observer && element.observer.off();
 
-        var module = element.tagName.toLowerCase();
-        var plainAttrs = xblocks.attrs.toPlainObject(element);
-        Object.merge(plainAttrs, element.defaultAttrs || {});
+
+    function XBElement(node, params) {
+        this.node = node;
+        this.module = node.tagName.toLowerCase();
+        this.schema = params.schema;
+        this.defaultAttrs = params.defaultAttrs;
+
+        this.observeStart();
+
+        this.on('inserted', function(/*event*/) {
+            xblocks.log('[field]', 'inserted', this);
+        });
+
+        this.on('removed', function(/*event*/) {
+            /*xblocks.log('[field]', 'removed', this);
+             this.observer.remove();
+             xtag.removeEvents(xblocks.rootElement(this), this.__events);
+             delete this.__events;
+             delete this.__controller;*/
+        });
+
+        this.on('attributeChanged', function(/*attrName, oldValue, newValue*/) {
+            if (this.__lock) {
+                xblocks.log('[field]', 'attributeChanged', 'lock', arguments);
+                return;
+            }
+
+            xblocks.log('[field]', 'attributeChanged', this, arguments);
+            this.update();
+        });
+    }
+
+    var proto = XBElement.prototype;
+
+    proto.isLock = function() {
+
+    };
+
+    proto.on = function() {
+
+    };
+
+    proto.off = function() {
+
+    };
+
+    proto.trigger = function() {
+
+    };
+
+    proto.remove = function() {
+
+    };
+
+    proto.observeStart = function() {
+        if (!Modernizr.createshadowroot && !this._observer) {
+            var that = this;
+            this._observer = new MutationObserver(function() {
+                xblocks.log('XBElement->mutation', that);
+                that.update();
+            });
+        }
+
+        if (this._observer) {
+            this._observer.disconnect();
+            this._observer.observe(this.node, { childList: true, subtree: true, characterData: true });
+        }
+    };
+
+    proto.observeStop = function() {
+        if (this._observer) {
+            this._observer.disconnect();
+        }
+    };
+
+    proto.update = function() {
+        xblocks.log('XBElement->update', this);
+        xblocks.log.time('XBElement->update');
+
+        this.observeStop();
+
+        var plainAttrs = xblocks.attrs.toPlainObject(this.node);
+        Object.merge(plainAttrs, this.defaultAttrs || {});
 
         var complexAttrs = plainAttrs.toComplex();
         if (!Modernizr.createshadowroot) {
-            complexAttrs.setValue(el.html(element));
+            complexAttrs.setValue(this.html());
         }
 
         var schemaAttrs = complexAttrs.toSchema();
 
-        if (tv4 && element.schema) {
-            var schema = tv4.getSchema(element.schema);
+        if (tv4 && this.schema) {
+            var schema = tv4.getSchema(this.schema);
             var check = tv4.validateResult(schemaAttrs, schema);
 
             if (!check.valid) {
@@ -50,32 +110,62 @@
             }
         }
 
-        var html = yr.run(module, schemaAttrs, 'template');
+        var html = yr.run(this.module, schemaAttrs, 'template');
         var template = xtag.createFragment(html);
+        var root = this.root();
+
+        xtag.innerHTML(root, '');
+        root.appendChild(template.cloneNode(true));
+
+        this.observeStart();
+
+        xblocks.log.timeEnd('XBElement->update');
+
+        //if (onupdate) {
+        //    onupdate(this.node);
+        //}
+    };
+
+    proto.html = function(html) {
+        if (typeof html !== 'undefined') {
+            this.node.innerHTML = html;
+
+            if (!Modernizr.createshadowroot) {
+                this.update();
+            }
+        }
+
+        var content;
+        if (!Modernizr.createshadowroot) {
+            content = this.node.querySelector('content');
+        }
+
+        return content && content.innerHTML || this.node.innerHTML;
+    };
+
+    proto.root = function() {
         var root;
 
         if (Modernizr.createshadowroot) {
-            root = element.shadowRoot;
+            root = this.node.shadowRoot;
             if (!root) {
-                root = element.createShadowRoot();
+                root = this.node.createShadowRoot();
                 root.resetStyleInheritance = true;
                 root.applyAuthorStyles = true;
             }
 
         } else {
-            root = element;
+            root = this.node;
         }
 
-        xtag.innerHTML(root, '');
-        root.appendChild(template.cloneNode(true));
-
-        element.observer && element.observer.on();
-        xblocks.log.timeEnd('element->update');
-
-        if (onupdate) {
-            onupdate(element);
-        }
+        return root;
     };
+
+
+
+
+
+
 
 
 })(xtag, xblocks, Modernizr, tv4, yr);
