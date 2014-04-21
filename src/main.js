c7ca1325daad01f423b8e6118e808ed9552f6e23
Katochimoto <bufpost@yandex.ru>
2014-04-21 21:26:35 +0400

    remove dependences

--- src/main.js
+++ src/main.js
@@ -1,30 +1,11 @@
 (function() {
-    (function() {
-        /*! borschik:include:../bower_components/es5-shim/es5-shim.js */
-    }.call(this));
 
     (function() {
         /*! borschik:include:../bower_components/modernizr.js */
     }.call(this));
 
     (function() {
-        /*! borschik:include:../bower_components/lodash/dist/lodash.js */
-    }.call(this));
-
-    (function() {
-        /*! borschik:include:../bower_components/react/react-with-addons.js */
-    }.call(this));
-
-    (function() {
         /*! borschik:include:../node_modules/borschik/js/borschik.js */
-    }.call(this));
-
-    (function() {
-        /*! borschik:include:../bower_components/x-tag-core/dist/x-tag-core.js */
-    }.call(this));
-
-    (function() {
-        /*! borschik:include:../bower_components/xblocks-core/xblocks.js */
     }.call(this));
 
     /*! borschik:include:blocks.js */
