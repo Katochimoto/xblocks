02d30b417abdd982b73c521f621981fddf368113
Katochimoto <bufpost@yandex.ru>
2014-05-30 00:20:33 +0400

    prop disabled

--- src/blocks/button/button.js
+++ src/blocks/button/button.js
@@ -2,7 +2,21 @@
     /*! borschik:include:button.jsx.js */
 
     xblocks.create('xb-button', {
-        prototype: Object.create(HTMLButtonElement.prototype)
+        prototype: Object.create(HTMLButtonElement.prototype),
+        accessors: {
+            disabled: {
+                get: function() {
+                    return Boolean(xblocks.dom.attrs.getRealValue('disabled', this.getAttribute('disabled')));
+                },
+                set: function(isDisabled) {
+                    if (isDisabled) {
+                        this.setAttribute('disabled', 'disabled');
+                    } else {
+                        this.removeAttribute('disabled');
+                    }
+                }
+            }
+        }
     });
 
 }(xblocks));
