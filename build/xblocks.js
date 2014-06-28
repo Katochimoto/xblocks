f12e75b91be389fe0b38f13266b52fa7b5daf8bd
Katochimoto <bufpost@yandex.ru>
2014-06-28 19:28:05 +0400

    checked fix

--- build/xblocks.js
+++ build/xblocks.js
@@ -112,31 +112,6 @@ xblocks.mixin.eChecked = {
                     this.removeAttribute('checked');
                 }
             }
-            /*
-            get: function() {
-                if (this.xblock._isMountedComponent()) {
-                    return this.xblock._component.state.checked;
-
-                } else {
-                    var controlNode = this.querySelector('input');
-                    return (controlNode ? controlNode.checked : false);
-                }
-            },
-
-            set: function(value) {
-                if (this.xblock._isMountedComponent()) {
-                    this.xblock._component.setState({
-                        'checked': Boolean(value)
-                    });
-
-                } else {
-                    var controlNode = this.querySelector('input');
-                    if (controlNode) {
-                        controlNode.checked = Boolean(value);
-                    }
-                }
-            }
-            */
         }
     }
 };
