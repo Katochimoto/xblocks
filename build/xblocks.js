5dacec4e154e0d56a6692d5a4dd1fecb5862c778
Katochimoto <bufpost@yandex.ru>
2014-07-08 00:16:48 +0400

    xb-select

--- build/xblocks.js
+++ build/xblocks.js
@@ -1434,6 +1434,47 @@ xblocks.create('xb-radio', [
 /* global xblocks, global, React */
 /* jshint strict: false */
 
+var XBSelect = xblocks.view.register('xb-select', {
+    displayName: 'xb-select',
+
+    propTypes: {
+        'id': React.PropTypes.string,
+        'class': React.PropTypes.string,
+        'children': React.PropTypes.renderable,
+        'tabindex': React.PropTypes.string,
+        'disabled': React.PropTypes.bool
+    },
+
+    getDefaultProps: function() {
+        return {
+            'disabled': false,
+            'tabindex': '1'
+        };
+    },
+
+    render: function() {
+        var classes = {
+            'xb-select': true,
+            '_disabled': this.props.disabled
+        };
+
+        classes = React.addons.classSet(classes);
+
+        var tabIndex = this.props.tabindex;
+
+        if (this.props.disabled) {
+            tabIndex = '-1';
+        }
+
+        return (
+            React.DOM.span( {className:classes}, 
+                React.DOM.input( {className:"_controller"} ),
+                XBButton( {type:"inline"}),
+                React.DOM.span( {className:"_xb-select-dropdown"})
+            )
+        );
+    }
+});
 
 
 /* blocks/select/select.jsx.js end */
--- src/blocks/select/select.jsx
+++ src/blocks/select/select.jsx
@@ -2,4 +2,45 @@
 /* global xblocks, global, React */
 /* jshint strict: false */
 
+var XBSelect = xblocks.view.register('xb-select', {
+    displayName: 'xb-select',
+
+    propTypes: {
+        'id': React.PropTypes.string,
+        'class': React.PropTypes.string,
+        'children': React.PropTypes.renderable,
+        'tabindex': React.PropTypes.string,
+        'disabled': React.PropTypes.bool
+    },
+
+    getDefaultProps: function() {
+        return {
+            'disabled': false,
+            'tabindex': '1'
+        };
+    },
+
+    render: function() {
+        var classes = {
+            'xb-select': true,
+            '_disabled': this.props.disabled
+        };
+
+        classes = React.addons.classSet(classes);
+
+        var tabIndex = this.props.tabindex;
+
+        if (this.props.disabled) {
+            tabIndex = '-1';
+        }
+
+        return (
+            <span className={classes}>
+                <input className="_controller" />
+                <XBButton type="inline"></XBButton>
+                <span className="_xb-select-dropdown"></span>
+            </span>
+        );
+    }
+});
 
