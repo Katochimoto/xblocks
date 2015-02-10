64abfcb72d79fdf4a308250d3f8ee2f4d9fc5b3b
Katochimoto <bufpost@yandex.ru>
2015-02-10 23:52:04 +0300

    simple refactoring

--- build/xblocks.js
+++ build/xblocks.js
@@ -1346,10 +1346,11 @@ xblocks.utils.exportPropTypes = function(tagName) {
     var props = xblocks.utils.propTypes(tagName);
     var exportProps = {};
     var prefix = tagName + '-';
+    var p;
 
-    for (var p in props) {
-        if (props.hasOwnProperty(p) && p[0] !== '_') {
-            exportProps[prefix + p] = props[p];
+    for (p in props) {
+        if (props.hasOwnProperty(p) && p[ 0 ] !== '_') {
+            exportProps[ prefix + p ] = props[ p ];
         }
     }
 
