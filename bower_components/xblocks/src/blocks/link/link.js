7106540b8a3e4dbc3016e71b3ea5b654ea34f161
Katochimoto <bufpost@yandex.ru>
2015-02-14 18:25:48 +0300

    add jsdoc

--- bower_components/xblocks/src/blocks/link/link.js
+++
@@ -1,8 +0,0 @@
-/* global xblocks */
-/* jshint strict: false */
-
-/*! borschik:include:link.jsx.js */
-
-xblocks.create('xb-link', [
-    xblocks.mixin.eDisabled
-]);
--- bower_components/xblocks/src/utils/filterPropsPrefixLink.js
+++
@@ -1,6 +0,0 @@
-/* global xblocks */
-/* jshint strict: false */
-
-xblocks.utils.filterPropsPrefixLink = function(name) {
-    return xblocks.utils.REG_PROPS_PREFIX_LINK.test(name);
-};
