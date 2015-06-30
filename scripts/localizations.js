202031b25e0a5dfa4da8e37799422904f9506c7d
Katochimoto <bufpost@yandex.ru>
2015-06-30 22:08:51 +0300

    doc

---
+++ scripts/localizations.js
@@ -0,0 +1,12 @@
+String.toLocaleString({
+    'ru': {
+        '%Controls_link__title': 'Ссылка',
+        '%Controls_link__descr_p1': 'Тег <code>&lt;xb-link&gt;</code> предназначен для создания ссылок. В зависимости от присутствия атрибутов <code>name</code> или <code>href</code> тег <code>&lt;xb-link&gt;</code> устанавливает ссылку или якорь.',
+
+        '%Controls_icon__title': 'Иконка'
+    },
+
+    'en': {
+
+    }
+});
