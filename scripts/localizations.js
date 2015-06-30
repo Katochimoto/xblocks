14ba5f2c8692d6d6972308ef8dcd33fdeb3cb49a
Katochimoto <bufpost@yandex.ru>
2015-06-30 22:22:27 +0300

    doc

--- scripts/localizations.js
+++ scripts/localizations.js
@@ -3,7 +3,15 @@ String.toLocaleString({
         '%Controls_link__title': 'Ссылка',
         '%Controls_link__descr_p1': 'Тег <code>&lt;xb-link&gt;</code> предназначен для создания ссылок. В зависимости от присутствия атрибутов <code>name</code> или <code>href</code> тег <code>&lt;xb-link&gt;</code> устанавливает ссылку или якорь.',
 
-        '%Controls_icon__title': 'Иконка'
+        '%Controls_ico__title': 'Иконка',
+        '%Controls_ico__descr_p1': 'Тег <code>&lt;xb-ico&gt;</code> позволяет создавать элемент интерфейса в виде иконки.',
+        '%Controls_ico__descr_p2': 'Набор иконок заранее определен. Выбор нужной иконки осуществляется указанием атрибута <code>type</code>.',
+        '%Controls_ico__descr_p3': 'Для большинства иконок закрывающийся тег не требуется, в отличии от иконки с типом <code>notification</code>. Текст внутри тега либо значение атрибута <code>value</code> являются сообщением.',
+        '%Controls_ico__syntax': 'Синтаксис',
+        '%Controls_ico__syntax_example': 'текст сообщения',
+        '%Controls_ico__attrs': 'Атрибуты',
+        '%Controls_ico__example': 'Пример',
+        '%Controls_ico__more_example': 'Больше примеров иконок.'
     },
 
     'en': {
