cdbe2e45e2470c711b496f7e72dab04c0bc4c163
Katochimoto <bufpost@yandex.ru>
2015-07-02 22:39:31 +0300

    link doc

--- scripts/localizations.js
+++ scripts/localizations.js
@@ -3,15 +3,27 @@ String.toLocaleString({
         '%Controls_link__title': 'Ссылка',
         '%Controls_link__descr_p1': 'Тег <code>&lt;xb-link&gt;</code> предназначен для создания ссылок. В зависимости от присутствия атрибутов <code>name</code> или <code>href</code> тег <code>&lt;xb-link&gt;</code> устанавливает ссылку или якорь.',
 
+        '%Controls__syntax': 'Синтаксис',
+        '%Controls__attrs': 'Атрибуты',
+        '%Controls__example': 'Пример',
+
         '%Controls_ico__title': 'Иконка',
         '%Controls_ico__descr_p1': 'Тег <code>&lt;xb-ico&gt;</code> позволяет создавать элемент интерфейса в виде иконки.',
         '%Controls_ico__descr_p2': 'Набор иконок заранее определен. Выбор нужной иконки осуществляется указанием атрибута <code>type</code>.',
         '%Controls_ico__descr_p3': 'Для большинства иконок закрывающийся тег не требуется, в отличии от иконки с типом <code>notification</code>. Текст внутри тега либо значение атрибута <code>value</code> являются сообщением.',
-        '%Controls_ico__syntax': 'Синтаксис',
         '%Controls_ico__syntax_example': 'текст сообщения',
-        '%Controls_ico__attrs': 'Атрибуты',
-        '%Controls_ico__example': 'Пример',
-        '%Controls_ico__more_example': 'Больше примеров иконок.'
+        '%Controls_ico__more_example': 'Больше примеров иконок.',
+        '%Controls_ico__attrs_active': 'Позволяет использовать иконку как управляющий элемент. При наведении на иконку появится \'лапка\' и изменится цвет.',
+        '%Controls_ico__attrs_disabled': 'Иконка станет серой и перестанет реагировать на события мыши.',
+        '%Controls_ico__attrs_size': 'Размер иконки. Возможные значения: <code>s</code>, <code>m</code>.',
+        '%Controls_ico__attrs_value': 'Содержимое иконки. Актуально для типа <code>notification</code>.',
+        '%Controls_ico__attrs_type': 'Тип иконки. Возможные значения:',
+
+        '%Controls_link__more_example': 'Больше примеров ссылок.',
+        '%Controls_link__attrs_href': 'Задает адрес документа, на который следует перейти.',
+        '%Controls_link__attrs_name': 'Устанавливает имя якоря внутри документа.',
+        '%Controls_link__attrs_target': 'Имя окна или фрейма, куда браузер будет загружать документ.',
+        '%Controls_link__attrs_theme': 'Тема оформления. Возможные значения:'
     },
 
     'en': {
