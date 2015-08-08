1ff30f3e8e56b05e535823757ecd76844cbada0c
Katochimoto <bufpost@yandex.ru>
2015-08-08 22:22:53 +0300

    checkbox

--- scripts/localizations.js
+++ scripts/localizations.js
@@ -6,7 +6,7 @@ String.toLocaleString({
         '%Controls__example': 'Пример',
         '%Controls__more_example': 'Больше примеров.',
 
-        '%Controls_ico__title': 'Иконка',
+        '%Controls_ico__title': 'Ico',
         '%Controls_ico__descr_p1': 'Тег <code>&lt;xb-ico&gt;</code> позволяет создавать элемент интерфейса в виде иконки.',
         '%Controls_ico__descr_p2': 'Набор иконок заранее определен. Выбор нужной иконки осуществляется указанием атрибута <code>type</code>.',
         '%Controls_ico__descr_p3': 'Для большинства иконок закрывающийся тег не требуется, в отличии от иконки с типом <code>notification</code>. Текст внутри тега либо значение атрибута <code>value</code> являются сообщением.',
@@ -17,14 +17,14 @@ String.toLocaleString({
         '%Controls_ico__attrs_value': 'Содержимое иконки. Актуально для типа <code>notification</code>.',
         '%Controls_ico__attrs_type': 'Тип иконки. Возможные значения:',
 
-        '%Controls_link__title': 'Ссылка',
+        '%Controls_link__title': 'Link',
         '%Controls_link__descr_p1': 'Тег <code>&lt;xb-link&gt;</code> предназначен для создания ссылок. В зависимости от присутствия атрибутов <code>name</code> или <code>href</code> тег <code>&lt;xb-link&gt;</code> устанавливает ссылку или якорь.',
         '%Controls_link__attrs_href': 'Задает адрес документа, на который следует перейти.',
         '%Controls_link__attrs_name': 'Устанавливает имя якоря внутри документа.',
         '%Controls_link__attrs_target': 'Имя окна или фрейма, куда браузер будет загружать документ.',
         '%Controls_link__attrs_theme': 'Тема оформления. Возможные значения:',
 
-        '%Controls_button__title': 'Кнопка',
+        '%Controls_button__title': 'Button',
         '%Controls_button__descr_p1': 'Тег <code>&lt;xb-button&gt;</code> предназначен для создания кнопок.',
         '%Controls_button__descr_p2': 'В зависимости от атрибута <code>type</code> кнопка может быть ссылкой или переключателем.',
         '%Controls_button__attrs_name': 'Имя поля, предназначено для того, чтобы обработчик формы мог его идентифицировать.',
@@ -39,7 +39,10 @@ String.toLocaleString({
         '%Controls_button__attrs_target': 'Имя окна или фрейма, куда браузер будет загружать документ. Для типа <code>link</code>.',
         '%Controls_button__attrs_theme': 'Тема оформления. Возможные значения:',
         '%Controls_button__attrs_type': 'Тип кнопки. Возможные значения:',
-        '%Controls_button__attrs_value': 'Значение элемента.'
+        '%Controls_button__attrs_value': 'Значение элемента.',
+
+        '%Controls_checkbox__title': 'Checkbox',
+        '%Controls_checkbox__descr_p1': 'Тег <code>&lt;xb-checkbox&gt;</code> предназначен для создания переключателя.'
     },
 
     'en': {
