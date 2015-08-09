9f72610a83347a829164964c607952ff1f592195
Katochimoto <bufpost@yandex.ru>
2015-08-09 12:27:51 +0300

    input

--- scripts/localizations.js
+++ scripts/localizations.js
@@ -6,13 +6,19 @@ String.toLocaleString({
         '%Controls__example': 'Пример',
         '%Controls__more_example': 'Больше примеров.',
 
+        '%Controls__attrs_autocomplete': 'Включает или отключает автозаполнение. Возможные значения: <code>on</code>, <code>off</code>.',
         '%Controls__attrs_autofocus': 'Устанавливает фокус в поле формы.',
         '%Controls__attrs_checked': 'Предварительно активированный переключатель или флажок.',
         '%Controls__attrs_form': 'Связывает поле с формой по её идентификатору.',
         '%Controls__attrs_for': 'Идентификатор элемента, с которым следует установить связь.',
         '%Controls__attrs_name': 'Имя поля, предназначено для того, чтобы обработчик формы мог его идентифицировать.',
+        '%Controls__attrs_readonly': 'Устанавливает, что поле не может изменяться пользователем.',
         '%Controls__attrs_required': 'Обязательное для заполнения поле.',
         '%Controls__attrs_value': 'Значение элемента.',
+        '%Controls__attrs_placeholder': 'Замещающийся текст.',
+        '%Controls__attrs_xb-link': 'Наследование атрибутов <code>&lt;xb-link&gt;</code> через префикс <code>xb-link</code>.',
+        '%Controls__attrs_xb-ico': 'Наследование атрибутов <code>&lt;xb-ico&gt;</code> через префикс <code>xb-ico</code>.',
+        '%Controls__attrs_xb-ico-float': 'Расположение иконки. Возможные значения: <code>left</code>, <code>right</code>.',
 
         '%Controls_ico__title': 'Ico',
         '%Controls_ico__descr_p1': 'Тег <code>&lt;xb-ico&gt;</code> позволяет создавать элемент интерфейса в виде иконки.',
@@ -47,7 +53,20 @@ String.toLocaleString({
         '%Controls_checkbox__attrs_size': 'Размер флажка. Возможные значения: <code>s</code>, <code>m</code>.',
 
         '%Controls_radio__title': 'Radio',
-        '%Controls_radio__descr_p1': 'Тег <code>&lt;xb-radio&gt;</code> предназначен для создания переключателя.'
+        '%Controls_radio__descr_p1': 'Тег <code>&lt;xb-radio&gt;</code> предназначен для создания переключателя.',
+
+        '%Controls_input__title': 'Input',
+        '%Controls_input__descr_p1': 'Тег <code>&lt;xb-input&gt;</code> предназначен для создания поля ввода.',
+        '%Controls_input__attrs_autosize': 'Изменять размер поля по содержимому.',
+        '%Controls_input__attrs_cols': 'Ширина поля в символах.',
+        '%Controls_input__attrs_ghost': 'Показывать границы поля при наведении курсора.',
+        '%Controls_input__attrs_multiline': 'Многострочный ввод текста.',
+        '%Controls_input__attrs_postfix': 'Текст после области ввода.',
+        '%Controls_input__attrs_prefix': 'Текст перед областью ввода.',
+        '%Controls_input__attrs_reset': 'Выводит кнопку сброса содержимого поля.',
+        '%Controls_input__attrs_rows': 'Высота поля в строках текста.',
+        '%Controls_input__attrs_size': 'Размер поля ввода. Возможные значения: <code>s</code>, <code>m</code>, <code>l</code>, <code>xl</code>.',
+        '%Controls_input__attrs_xb-link': 'Название ссылки.'
     },
 
     'en': {
